e2175dab7b20f62930db1022775db88d2a24e349
Incbom <99501939+IncbomDev@users.noreply.github.com>
2023-01-12 14:45:13 -0500

    Update achivement.js

--- main/achivement.js
+++ main/achivement.js
@@ -1,1 +1,60 @@
+<html>
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
 
+.callout {
+  position: fixed;
+  bottom: 35px;
+  right: 20px;
+  margin-left: 20px;
+  max-width: 300px;
+}
+
+.callout-header {
+  padding: 25px 15px;
+  background: #555;
+  font-size: 30px;
+  color: white;
+}
+
+.callout-container {
+  padding: 15px;
+  background-color: #ccc;
+  color: black
+}
+
+.closebtn {
+  position: absolute;
+  top: 5px;
+  right: 15px;
+  color: white;
+  font-size: 30px;
+  cursor: pointer;
+}
+
+.closebtn:hover {
+  color: lightgrey;
+}
+</style>
+
+
+
+<div class="callout">
+  <div class="callout-header">Callout Header</div>
+  <span class="closebtn" onclick="this.parentElement.style.display='none';">×</span>
+  <div class="callout-container">
+    <p>Some text to describe the callout message. </p>
+  </div>
+</div>
+<script>
+if(weight==100000){
+  <div class="callout">
+  <div class="callout-header">Achivement!</div>
+  <span class="closebtn" onclick="this.parentElement.style.display='none';">×</span>
+  <div class="callout-container">
+  <p>Weight enthusiest (100k weight)</p>
+  </div>
+</div>
+
+</body>
+</html>
